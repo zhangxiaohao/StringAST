e09824a27b1bd4c104d61ebbf738b4d0cc831817
zhangxiaohao <915628768@qq.com>
2016-09-23 10:51:47 +0800

    单个站点调试通过

--- src/cn/edu/fudan/stringast/StringASTTest.java
+++ src/cn/edu/fudan/stringast/StringASTTest.java
@@ -17,8 +17,8 @@ public class StringASTTest {
     public static ArrayList<S_Generator> s_generators = new ArrayList<S_Generator>();
     public static ArrayList<S_Executor> s_executors = new ArrayList<S_Executor>();
     public static ArrayList<S_Sender> s_senders = new ArrayList<S_Sender>();
-    public static final int SITE = 1;
-    public static final int OPNUM = 5;
+    public static final int SITE = 10;
+    public static final int OPNUM = 10;
 
     public static void run() throws InterruptedException {
         for(int i=0; i<SITE; i++) s_algorithms.add(new S_Algorithm(i, SITE));
@@ -26,21 +26,21 @@ public class StringASTTest {
         for(int i=0; i<SITE; i++) {
             s_generators.add(new S_Generator(s_algorithms.get(i), OPNUM));
             s_generators.get(i).start();
-            //s_senders.add(new S_Sender(i, s_algorithms));
-            //s_senders.get(i).start();
-            //s_executors.add(new S_Executor(s_algorithms.get(i)));
-            //s_executors.get(i).start();
+            s_senders.add(new S_Sender(i, s_algorithms));
+            s_senders.get(i).start();
+            s_executors.add(new S_Executor(s_algorithms.get(i)));
+            s_executors.get(i).start();
         }
         Thread.sleep(3000);
         for(int i=0; i<SITE; i++) {
             s_generators.get(i).stop();
-            //s_senders.get(i).stop();
-            //s_executors.get(i).stop();
+            s_senders.get(i).stop();
+            s_executors.get(i).stop();
         }
         for(S_Algorithm s_algorithm : s_algorithms) {
-            //s_algorithm.printModel();
-            s_algorithm.printModelinDetail();
-            System.out.println("***-------------------------------***");
+            s_algorithm.printModel();
+            //s_algorithm.printModelinDetail();
+            //System.out.println("***-------------------------------***");
         }
     }
 
